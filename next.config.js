0bdfede9be31ef79392c4bf2b5c22b9c998c8587
dominik2323 <dominik.tomcik23@gmail.com>
2019-10-15 13:27:00 +0200

    change cdn

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const prefix =
   process.env.NODE_ENV === 'production'
-    ? 'https://steezycdn.tomczik.cz'
+    ? 'https://steezy.studio'
     : 'http://localhost:3000';
 module.exports = withCSS(
   withSass({
